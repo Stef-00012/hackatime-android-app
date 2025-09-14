405f15ffac6e2f2bea1bad9852404c9e7939edcc
Stef-00012 <me@stefdp.com>
2025-09-14 20:18:31 +0200

    update flake and icons

--- flake.nix
+++ flake.nix
@@ -2,26 +2,30 @@
     description = "Creates an environment in which you can create, build and emulate an expo app, and a script which creates, builds and emulates an expo app.";
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs?rev=55d15ad12a74eb7d4646254e13638ad0c4128776";
+        nixpkgs.url = "github:nixos/nixpkgs";
+        # nixpkgs.url = "github:nixos/nixpkgs?rev=55d15ad12a74eb7d4646254e13638ad0c4128776";
         #nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable&rev=55d15ad12a74eb7d4646254e13638ad0c4128776";
 
-        android.url = "github:tadfisher/android-nixpkgs?rev=a21442ddcdf359be82220a6e82eff7432d9a4190";
+        android.url = "github:tadfisher/android-nixpkgs";
+        # android.url = "github:tadfisher/android-nixpkgs?rev=a21442ddcdf359be82220a6e82eff7432d9a4190";
         #android.url = "github:tadfisher/android-nixpkgs/stable?rev=a21442ddcdf359be82220a6e82eff7432d9a4190";
         android.inputs.nixpkgs.follows = "nixpkgs";
     };
 
     outputs = { self, nixpkgs, ...}@inputs: 
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux; 
+    let 
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        latest-build-tools = "36.0.0";
     in {
         packages.x86_64-linux.android-sdk = inputs.android.sdk.x86_64-linux (sdkPkgs: [
             sdkPkgs.cmdline-tools-latest
             sdkPkgs.cmake-3-22-1
-            sdkPkgs.build-tools-34-0-0
             sdkPkgs.build-tools-35-0-0
+            sdkPkgs.build-tools-36-0-0
             sdkPkgs.platform-tools
-            sdkPkgs.platforms-android-35
+            sdkPkgs.platforms-android-36
             sdkPkgs.emulator
-            sdkPkgs.system-images-android-35-google-apis-x86-64 
+            sdkPkgs.system-images-android-36-google-apis-x86-64 
             sdkPkgs.ndk-27-1-12297006
         ]);
 
@@ -35,7 +39,7 @@
         mkdir -p demo-app-$date/avds
         export ANDROID_AVD_HOME=$HOME/.config/.android/avd
         echo using $ANDROID_AVD_HOME as ANDROID_AVD_HOME
-        echo "no" | avdmanager create avd -k 'system-images;android-35;google_apis;x86_64' -n $avd
+        echo "no" | avdmanager create avd -k 'system-images;android-36;google_apis;x86_64' -n $avd
         cd demo-app-$date
         echo "demo-app" | yarn create expo-app
         cd demo-app
@@ -47,11 +51,11 @@
             JAVA_HOME=pkgs.corretto17.home;
             ANDROID_HOME="${self.packages.x86_64-linux.android-sdk}/share/android-sdk";
             ANDROID_SDK_ROOT="${self.packages.x86_64-linux.android-sdk}/share/android-sdk";
-            GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/34.0.0/aapt2";
+            GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/${latest-build-tools}/aapt2";
             ANDROID_AVD_HOME = "~/.config/.android/avd";
 
             nativeBuildInputs = [ 
-                pkgs.nodejs_18 
+                pkgs.nodejs_20
                 pkgs.yarn
                 pkgs.bun
                 pkgs.watchman
