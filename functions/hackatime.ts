aeeb24de078e188fcaf53aca26949f8b0d940612
Stef-00012 <me@stefdp.com>
2025-09-15 22:32:50 +0200

    format code

--- functions/hackatime.ts
+++ functions/hackatime.ts
@@ -2,19 +2,20 @@ import * as db from "@/functions/database";
 import axios from "axios";
 
 interface GetUserStatsParams {
-    startDate?: `${number}-${number}-${number}`;
-    endDate?: `${number}-${number}-${number}`;
-    features?: ("languages" | "projects")[];
+	startDate?: `${number}-${number}-${number}`;
+	endDate?: `${number}-${number}-${number}`;
+	features?: ("languages" | "projects")[];
 }
 
 export async function getUserStats(params?: GetUserStatsParams) {
 	const apiKey = db.get("api_key");
 
-    const searchParams = new URLSearchParams()
+	const searchParams = new URLSearchParams();
 
-    if (params?.startDate) searchParams.append("start_date", params.startDate);
-    if (params?.endDate) searchParams.append("end_date", params.endDate);
-    if (params?.features) searchParams.append("features", params.features.join(","));
+	if (params?.startDate) searchParams.append("start_date", params.startDate);
+	if (params?.endDate) searchParams.append("end_date", params.endDate);
+	if (params?.features)
+		searchParams.append("features", params.features.join(","));
 
 	try {
 		const res = await axios.get(
@@ -30,7 +31,7 @@ export async function getUserStats(params?: GetUserStatsParams) {
 
 		return data;
 	} catch (_e) {
-        console.error(_e)
+		console.error(_e);
 		return null;
 	}
 }
