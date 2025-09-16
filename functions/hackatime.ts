0e10d56874411a230c3570171dc241c272ab8b1f
Stef-00012 <me@stefdp.com>
2025-09-16 18:58:00 +0200

    add more functions and types

--- functions/hackatime.ts
+++ functions/hackatime.ts
@@ -1,4 +1,5 @@
 import * as db from "@/functions/database";
+import type { CurrentUserRawHeartbeats, UserStatsResponse, UserTodayDataResponse, UserTrustFactor } from "@/types/hackatime";
 import axios from "axios";
 
 interface GetUserStatsParams {
@@ -7,8 +8,10 @@ interface GetUserStatsParams {
 	features?: ("languages" | "projects")[];
 }
 
-export async function getUserStats(params?: GetUserStatsParams) {
+export async function getCurrentUserStats(params?: GetUserStatsParams): Promise<UserStatsResponse | null> {
 	const apiKey = db.get("api_key");
+
+	if (!apiKey) return null;
 
 	const searchParams = new URLSearchParams();
 
@@ -20,6 +23,195 @@ export async function getUserStats(params?: GetUserStatsParams) {
 	try {
 		const res = await axios.get(
 			"https://hackatime.hackclub.com/api/v1/users/my/stats",
+			{
+				headers: {
+					Authorization: `Bearer ${apiKey}`,
+				},
+			},
+		);
+
+		const data = res.data;
+
+		return data;
+	} catch (_e) {
+		console.error(_e);
+		return null;
+	}
+}
+
+export async function getUserStats(userId: string, params?: GetUserStatsParams): Promise<UserStatsResponse | null> {
+	const apiKey = db.get("api_key");
+
+	if (!apiKey) return null;
+
+	const searchParams = new URLSearchParams();
+
+	if (params?.startDate) searchParams.append("start_date", params.startDate);
+	if (params?.endDate) searchParams.append("end_date", params.endDate);
+	if (params?.features)
+		searchParams.append("features", params.features.join(","));
+
+	try {
+		const res = await axios.get(
+			`https://hackatime.hackclub.com/api/v1/users/${userId}/stats`,
+			{
+				headers: {
+					Authorization: `Bearer ${apiKey}`,
+				},
+			},
+		);
+
+		const data = res.data;
+
+		return data;
+	} catch (_e) {
+		console.error(_e);
+		return null;
+	}
+}
+
+export async function getCurrentUserTodayData(): Promise<UserTodayDataResponse | null> {
+	const apiKey = db.get("api_key");
+
+	if (!apiKey) return null;
+
+	try {
+		const res = await axios.get(
+			`https://hackatime.hackclub.com/api/hackatime/v1/users/current/statusbar/today`,
+			{
+				headers: {
+					Authorization: `Bearer ${apiKey}`,
+				},
+			},
+		);
+
+		const data = res.data;
+
+		return data;
+	} catch (_e) {
+		console.error(_e);
+		return null;
+	}
+}
+
+export async function getUserTodayData(userId: string): Promise<UserTodayDataResponse | null> {
+	const apiKey = db.get("api_key");
+
+	if (!apiKey) return null;
+
+	try {
+		const res = await axios.get(
+			`https://hackatime.hackclub.com/api/hackatime/v1/users/${userId}/statusbar/today`,
+			{
+				headers: {
+					Authorization: `Bearer ${apiKey}`,
+				},
+			},
+		);
+
+		const data = res.data;
+
+		return data;
+	} catch (_e) {
+		console.error(_e);
+		return null;
+	}
+}
+
+export async function getUserTrustFactor(userId: string): Promise<UserTrustFactor | null> {
+	const apiKey = db.get("api_key");
+
+	if (!apiKey) return null;
+
+	try {
+		const res = await axios.get(
+			`https://hackatime.hackclub.com/api/v1/users/${userId}/trust_factor`,
+			{
+				headers: {
+					Authorization: `Bearer ${apiKey}`,
+				},
+			},
+		);
+
+		const data = res.data;
+
+		return data;
+	} catch (_e) {
+		console.error(_e);
+		return null;
+	}
+}
+
+interface GetCurrentUserRawHeartbeatsParams {
+	startDate?: `${number}-${number}-${number}`;
+	endDate?: `${number}-${number}-${number}`;
+	limit?: number; // max 100
+	mostRecent?: boolean;
+}
+
+export async function getCurrentUserRawHeartbeats(params?: GetCurrentUserRawHeartbeatsParams): Promise<CurrentUserRawHeartbeats | null> {
+	const apiKey = db.get("api_key");
+
+	if (!apiKey) return null;
+
+	const searchParams = new URLSearchParams();
+
+	if (params?.startDate) searchParams.append("start_date", params.startDate);
+	if (params?.endDate) searchParams.append("end_date", params.endDate);
+	if (params?.limit)
+		searchParams.append("limit", String(params.limit));
+
+	try {
+		const res = await axios.get(
+			`https://hackatime.hackclub.com/api/v1/my/heartbeats${params?.mostRecent ? "/most_recent" : ""}?${searchParams}`,
+			{
+				headers: {
+					Authorization: `Bearer ${apiKey}`,
+				},
+			},
+		);
+
+		const data = res.data;
+
+		return data;
+	} catch (_e) {
+		console.error(_e);
+		return null;
+	}
+}
+
+export async function getUserProjects(userId: string, detailed = false) {
+	const apiKey = db.get("api_key");
+
+	if (!apiKey) return null;
+
+	try {
+		const res = await axios.get(
+			`https://hackatime.hackclub.com/api/v1/users/${userId}/projects${detailed ? "/details" : ""}`,
+			{
+				headers: {
+					Authorization: `Bearer ${apiKey}`,
+				},
+			},
+		);
+
+		const data = res.data;
+
+		return data;
+	} catch (_e) {
+		console.error(_e);
+		return null;
+	}
+}
+
+export async function getCurrentUserProjects(detailed = false) {
+	const apiKey = db.get("api_key");
+
+	if (!apiKey) return null;
+
+	try {
+		const res = await axios.get(
+			`https://hackatime.hackclub.com/api/v1/users/my/projects${detailed ? "/details" : ""}`,
 			{
 				headers: {
 					Authorization: `Bearer ${apiKey}`,
--- types/hackatime.ts
+++ types/hackatime.ts
@@ -1,35 +1,109 @@
-export interface SelfUserStatsResponse {
-    data: {
-        username: string;
-        user_id: string;
-        is_coding_activity_visible: boolean;
-        is_other_usage_visible: boolean;
-        status: string;
-        start: string;
-        end: string;
-        range: string;
-        human_readable_range: string;
-        total_seconds: number;
-        daily_average: number;
-        human_readable_total: string;
-        human_readable_daily_average: string;
-        languages?: Language[];
-        projects?: Project[];
-        trust_factor: {
-            trust_level: "green" | "blue" | "red";
-            trust_value: number;
-        }
-    }
+export interface UserStatsResponse {
+	data: {
+		username: string;
+		user_id: string;
+		is_coding_activity_visible: boolean;
+		is_other_usage_visible: boolean;
+		status: string;
+		start: string;
+		end: string;
+		range: string;
+		human_readable_range: string;
+		total_seconds: number;
+		daily_average: number;
+		human_readable_total: string;
+		human_readable_daily_average: string;
+		languages?: Language[];
+		projects?: Project[];
+		trust_factor: UserTrustFactor;
+	};
 }
 
 interface Language {
-    name: string;
-    total_seconds: number;
-    text: string;
-    hours: number;
-    minutes: number;
-    percent: number;
-    digital: string;
+	name: string;
+	total_seconds: number;
+	text: string;
+	hours: number;
+	minutes: number;
+	percent: number;
+	digital: string;
 }
 
 export type Project = Language;
+
+export interface UserTodayDataResponse {
+	data: {
+		grand_total: {
+			text: string;
+			total_seconds: number;
+		};
+	};
+}
+
+export interface UserTrustFactor {
+	trust_level: "green" | "blue" | "red";
+	trust_value: number;
+}
+
+export interface CurrentUserRawHeartbeats {
+	start_time: string;
+	end_time: string;
+	total_seconds: number;
+	heartbeats: Heartbeat[];
+}
+
+export interface Heartbeat {
+	id: number;
+	user_id: number;
+	branch: string;
+	category: string;
+	dependencies: [];
+	editor: string;
+	entity: string;
+	language: string;
+	machine: string;
+	operating_system: string;
+	project: string;
+	type: string;
+	user_agent: string;
+	line_additions: null;
+	line_deletions: null;
+	lineno: number;
+	lines: number;
+	cursorpos: number;
+	project_root_count: number;
+	time: number;
+	is_write: null;
+	created_at: string;
+	updated_at: string;
+	fields_hash: string;
+	source_type: string;
+	ip_address: string;
+	ysws_program: string;
+	deleted_at: null;
+	raw_data: HeartbeatRawData;
+	raw_heartbeat_upload_id: number;
+}
+
+export interface HeartbeatRawData {
+    time: number;
+    type: string;
+    lines: number;
+    branch: string;
+    editor: string;
+    entity: string;
+    lineno: number;
+    machine: string;
+    project: string;
+    user_id: number;
+    category: string;
+    is_write: null;
+    language: string;
+    cursorpos: number;
+    user_agent: string;
+    dependencies: [];
+    line_additions: null;
+    line_deletions: null;
+    operating_system: string;
+    project_root_count: number;
+};
