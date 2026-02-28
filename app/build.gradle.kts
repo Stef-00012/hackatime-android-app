5a1b67d70fe76d3cb8cac3c22f18e807f7dc90c8
Stef-00012 <me@stefdp.com>
2026-02-28 13:46:01 +0100

    added all frames of the splash icon to make it loopable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.stefdp.hackatime"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "2.0.3"
+        versionCode = 15
+        versionName = "2.0.4"
         ndkVersion = "29.0.14206865"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
