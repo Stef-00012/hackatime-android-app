11a9592f65b5190ff0facf470fd701dd90048226
Stef-00012 <me@stefdp.com>
2026-08-29 23:22:17 +0200

    feat: add custom `Logger` class

--- app/src/debug/java/com/stefdp/hackatime/DebugWrapper.kt
+++ app/src/debug/java/com/stefdp/hackatime/DebugWrapper.kt
@@ -1,5 +1,6 @@
 package com.stefdp.hackatime
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 
 const val IS_DEBUG = true
@@ -8,4 +9,32 @@ const val DEBUG_NETWORK = false
 @Composable
 fun DebugWrapper(content: @Composable () -> Unit) {
     content()
+}
+
+class Logger {
+    companion object {
+        fun debug(tag: String, message: String, throwable: Throwable? = null) {
+            Log.d(tag, message, throwable)
+        }
+
+        fun error(tag: String, message: String, throwable: Throwable? = null) {
+            Log.e(tag, message, throwable)
+        }
+
+        fun info(tag: String, message: String, throwable: Throwable? = null) {
+            Log.i(tag, message, throwable)
+        }
+
+        fun warn(tag: String, message: String, throwable: Throwable? = null) {
+            Log.w(tag, message, throwable)
+        }
+
+        fun verbose(tag: String, message: String, throwable: Throwable? = null) {
+            Log.v(tag, message, throwable)
+        }
+
+        fun wtf(tag: String, message: String, throwable: Throwable? = null) {
+            Log.wtf(tag, message, throwable)
+        }
+    }
 }
--- app/src/release/java/com/stefdp/hackatime/DebugWrapper.kt
+++ app/src/release/java/com/stefdp/hackatime/DebugWrapper.kt
@@ -4,5 +4,22 @@ import androidx.compose.runtime.Composable
 
 const val IS_DEBUG = false
 const val DEBUG_NETWORK = false
+
 @Composable
 fun DebugWrapper(content: @Composable () -> Unit) {}
+
+class Logger {
+    companion object {
+        fun debug(tag: String, message: String, throwable: Throwable? = null) {}
+
+        fun error(tag: String, message: String, throwable: Throwable? = null) {}
+
+        fun info(tag: String, message: String, throwable: Throwable? = null) {}
+
+        fun warn(tag: String, message: String, throwable: Throwable? = null) {}
+
+        fun verbose(tag: String, message: String, throwable: Throwable? = null) {}
+
+        fun wtf(tag: String, message: String, throwable: Throwable? = null) {}
+    }
+}
